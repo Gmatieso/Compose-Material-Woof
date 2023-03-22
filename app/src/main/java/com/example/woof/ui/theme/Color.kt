5e01a9a6535a711d6612653315ec1a01aa7ea9e1
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-22 09:31:27 +0300

    shape dogs image into circle

--- app/src/main/java/com/example/woof/ui/theme/Color.kt
+++ app/src/main/java/com/example/woof/ui/theme/Color.kt
@@ -26,15 +26,6 @@ val Grey700 = Color(0xFF5F6368)
 val Green50 = Color(0xFFE6F4EA)
 val Green100 = Color(0xFFCEEAD6)
 
-private val LightColorPalette = lightColors(
-    background = Green100,
-    surface = Green50,
-    onSurface = Grey900,
-    primary = Grey50,
-    onPrimary = Grey900,
-    secondary = Grey700
-)
-
 
 //Dark Theme
 val White = Color(0xFFFFFFFF)
@@ -42,13 +33,5 @@ val Grey100 = Color(0xFFF1F3F4)
 val Cyan900 = Color(0xFF007B83)
 val Cyan700 = Color(0xFF129EAF)
 
-private val DarkColorPalette = darkColors(
-    background = Cyan900,
-    surface = Cyan700,
-    onSurface = White,
-    primary = Grey900,
-    onPrimary = White,
-    secondary = Grey100
-)
 
 

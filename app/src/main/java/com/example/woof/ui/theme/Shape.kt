38c75a7efe359357d8dad6a6c3b44ffea23aed0e
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-22 09:42:19 +0300

    Adding a shape to the list item

--- app/src/main/java/com/example/woof/ui/theme/Shape.kt
+++ app/src/main/java/com/example/woof/ui/theme/Shape.kt
@@ -19,8 +19,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
+
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(16.dp),
+    medium = RoundedCornerShape(16.dp), //card is a medium component
     large = RoundedCornerShape(0.dp)
 )
